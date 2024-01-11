02f6fb7179b3af14d6852a9f307b886346eeb60e
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2024-01-11 22:44:29 +0100

    added config for secure file uploads, added config for more secure session

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,17 @@
 import os
+from datetime import timedelta
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
+
+    # Cookie session config
+    SESSION_COOKIE_SECURE = True
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
+
+    # File upload config
+    UPLOAD_PATH = os.environ.get("UPLOAD_FOLDER")
+    UPLOAD_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif', '.pdf']
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024 # 16MB
+
+
