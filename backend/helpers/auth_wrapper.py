49998dd6c9a3e3d7e15419aa8eb4c972ce81dba2
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2024-01-12 19:41:06 +0100

    changed auth_wrapper.py

--- backend/helpers/auth_wrapper.py
+++ backend/helpers/auth_wrapper.py
@@ -1,10 +1,10 @@
 from functools import wraps
-from flask import jsonify, session
+from flask import jsonify, session, redirect, url_for
 
 def requires_authentication(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
         if "user_id" not in session or "authenticated" not in session or not session["authenticated"]:
-            return jsonify({"error": "Unauthorized"}), 401
+            return redirect(url_for('test', _method="GET"))
         return func(*args, **kwargs)
     return decorated_function
