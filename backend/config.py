beec79951898f3aefbce5ad0e092c3d5e9069cef
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2024-01-14 09:49:13 +0100

    added config values

--- backend/config.py
+++ backend/config.py
@@ -13,14 +13,16 @@ class Config:
     # File upload config
     UPLOAD_PATH = os.environ.get("UPLOAD_PATH")
     UPLOAD_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.pdf']
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024 # 16MB
+    MAX_CONTENT_LENGTH = 2 * 1024 * 1024  # 2MB, max size used for request
+    MAX_PIL_IMAGE_PIXELS = 16 * 1024 * 1024  # 4000 x 4000 pixels, max size used for PIL
+
 
     # Login and register config
     MAX_FAILED_LOGIN_ATTEMPTS = 3
     AMOUNT_OF_CHARS_REQUIRED_IN_PASSWORD = 6
-    LOGIN_TIMEOUT = 2
-
     AMOUNT_OF_COMBINATIONS_GENERATED_FOR_PASSWORD = 10
+
+    # Timeout config (time in seconds)
+    LOGIN_TIMEOUT = 2
     REGISTER_TIMEOUT = 2
-
-
+    CHANGE_PASSWORD_TIMEOUT = 2
